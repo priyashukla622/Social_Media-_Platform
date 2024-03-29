c273dd5b229644c259e6952dff8347b63cbc30ca
priya shukla <priyashukla22@navgurukul.org>
2024-03-29 19:28:25 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express=require("express");
 const app = express();
-require('dotenv').config({path:'./src/.env'})
+require('dotenv').config({path:'./.env'})
 
 const userRouter = require("./src/routes/userRoutes");
 const mongoose = require("mongoose");
